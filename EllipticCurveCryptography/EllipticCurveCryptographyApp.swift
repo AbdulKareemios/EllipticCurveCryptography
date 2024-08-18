746e4f724f5e4f55ad423fc71f6cff5356e7d19b
AbdulKareemios <a.kareem.tn@gmail.com>
2024-08-18 16:00:57 +0500

    Initial Commit

---
+++ EllipticCurveCryptography/EllipticCurveCryptographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EllipticCurveCryptographyApp.swift
+//  EllipticCurveCryptography
+//
+//  Created by AK on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct EllipticCurveCryptographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
